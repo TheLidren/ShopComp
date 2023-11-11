1a16df5ebe76117a36189031163415e0cc1f4ca8
Владислав Мисевич <TheLidren@outlook.com>
2023-11-11 22:19:35 +0300

    Save

---
+++ ShopComp/ViewModels/Pages/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using ShopComp.Models;
+using System.Collections.Generic;
+
+namespace ShopComp.ViewModels
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<Tovar> Tovars { get; set; }
+        public PageViewModel PageViewModel { get; set; }
+        public FilterViewModel FilterViewModel { get; set; }
+    }
+}
